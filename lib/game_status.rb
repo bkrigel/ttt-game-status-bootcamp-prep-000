f47c08d11d148c0ddfb05530249807cebfec62a4
Benjamin Krigel <benjamin.krigel@powerhrg.com>
2018-05-02 23:56:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,11 +43,13 @@ def winner(board)
     board[winc[0]] == "X"
     end
     "X"
-  else WIN_COMBINATIONS.detect do |winc|
+  elsif WIN_COMBINATIONS.detect do |winc|
     board[winc[0]] == board[winc[1]] &&
     board[winc[1]] == board[winc[2]] &&
     board[winc[0]] == "O"
     end
     "O"
+  else
+    nil
   end
 end
